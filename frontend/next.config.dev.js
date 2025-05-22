afd7e8bfe904d2a775794ec2e4a6985f6931672a
Breachfix <Ntihanirahojacques@gmail.com>
2025-05-21 23:15:51 -0700

    ignore  CSP for dev and prod environments

--- frontend/next.config.dev.js
+++ frontend/next.config.dev.js
@@ -16,16 +16,16 @@ const nextDevConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-              default-src * data: blob:;
-              script-src * 'unsafe-inline' 'unsafe-eval' data: blob:;
-              style-src * 'unsafe-inline' * data:;
-              img-src * data: blob:;
-              font-src * data:;
-              connect-src * http://localhost:7001 http://localhost:3000 https://breachfixdb.onrender.com https://adventhub.onrender.com https://breachfix.com https://www.breachfix.com https://breachfix.ca https://www.breachfix.ca;
-              frame-src *;
-              media-src *;
-              object-src *;
-            `.replace(/\n/g, "").trim(),
+  default-src * data: blob:;
+  script-src * 'unsafe-inline' 'unsafe-eval' data: blob:;
+  style-src * 'unsafe-inline' data: blob:;
+  img-src * data: blob:;
+  font-src * data: blob:;
+  connect-src *;
+  frame-src *;
+  media-src *;
+  object-src *;
+`.replace(/\n/g, "").trim()
           },
         ],
       },
--- frontend/next.config.prod.js
+++ frontend/next.config.prod.js
@@ -16,16 +16,16 @@ const nextProdConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-              default-src 'self' data: blob: *;
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: *;
-              style-src 'self' 'unsafe-inline' data: blob: *;
-              img-src 'self' data: blob: https://image.tmdb.org https://d3rotgd7sghdsb.cloudfront.net https://bridgefixdb.s3.us-east-1.amazonaws.com https://firebasestorage.googleapis.com;
-              font-src 'self' data: blob:;
-              connect-src 'self' https://breachfixdb.onrender.com https://adventhub.onrender.com https://breachfix.com https://www.breachfix.com https://breachfix.ca https://www.breachfix.ca;
-              frame-src *;
-              media-src *;
-              object-src *;
-            `.replace(/\n/g, "").trim(),
+  default-src * data: blob:;
+  script-src * 'unsafe-inline' 'unsafe-eval' data: blob:;
+  style-src * 'unsafe-inline' data: blob:;
+  img-src * data: blob:;
+  font-src * data: blob:;
+  connect-src *;
+  frame-src *;
+  media-src *;
+  object-src *;
+`.replace(/\n/g, "").trim()
           },
         ],
       },
